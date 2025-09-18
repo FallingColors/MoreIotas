6a881bc5dad96c4fa4533722aae899845a4e8e1f
object-Object <object@objectobject.ca>
2025-09-18 12:35:39 -0400

    Remove unnecessary imports

--- buildSrc/src/main/kotlin/moreiotas/publish.gradle.kts
+++ buildSrc/src/main/kotlin/moreiotas/publish.gradle.kts
@@ -1,9 +1,5 @@
 package moreiotas
 
-import gradle.kotlin.dsl.accessors._63fbed089a48cacc8b61113b9deae027.publishMods
-import org.gradle.kotlin.dsl.assign
-import org.gradle.kotlin.dsl.provideDelegate
-
 plugins {
     id("me.modmuss50.mod-publish-plugin")
     id("moreiotas.utils.mod-dependencies")
